b8403d78627ad002395db9c9f96b769057f2a8ab
Vijay Kumar <87031722+vijay102879@users.noreply.github.com>
2023-03-26 00:42:09 +0530

    movie-search feature

---
+++ src/header.js
@@ -0,0 +1,13 @@
+import { Outlet, Link } from "react-router-dom";
+
+function Header(){
+    return(
+        <div>
+            <div className="title">
+                <h1 className="heading">Netflix Movie</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/moviedetails.js
@@ -0,0 +1,31 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function Details() {
+let params = useParams();
+//  In our React app sometimes we want to access the parameters of the current route in this case useParams hook comes into action. The react-router-dom package has useParams hooks that let you access the parameters of the current route.
+
+
+const [movie, setMovie] = useState();
+useEffect(() => {
+    fetch("http://www.omdbapi.com/?i="+params.movieid+"&apikey=142e1265")
+      .then((response) => response.json())
+      .then((data) => setMovie(data));
+  }, []);
+
+  console.log(movie);
+return(<div>
+    {movie ? <><h1 id="detail-title" style={{textAlign: "center"}}>Movie Details</h1>
+    <div style={{  width: "269px",margin: "auto"}}>
+        <img src={movie.Poster} />
+        <p>{movie.Title}</p>
+        <p>Actors : {movie.Actors}</p>
+        <p>Year: {movie.Released}</p>
+        <p> Ratings : {movie.imdbRating}</p>
+        <p>Runtime : {movie.Runtime}</p>
+        <p>{movie.Plot}</p>
+    </div></> : <h1>Loading</h1>}
+ </div>);   
+}
+
+export default Details;
